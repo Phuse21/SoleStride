ae5d5cc755f10f875ba8cd8d8754cc3e3913184f
Phuse21 <phusedallas@gmail.com>
2024-06-14 20:58:36 -0500

    radio buttons

--- resources/views/components/radio.blade.php
+++ resources/views/components/radio.blade.php
@@ -7,11 +7,13 @@ $options = [
 ];
 @endphp
 
-
-@foreach ($options as $optionLabel => $optionValue)
-<div class="form-check">
+<div class="flex border-2 border-teal-500 rounded-lg">
+    @foreach ($options as $optionLabel => $optionValue)
     <input wire:model.live="{{ $name }}" type="radio" name="{{ $name }}" id="{{ $optionValue }}"
-        value="{{ $optionValue }}">
-    <label for="{{ $name . '-' . $optionValue }}">{{ $optionLabel }}</label>
+        value="{{ $optionValue }}" class="hidden peer">
+    <label for="{{ $optionValue }}"
+        class="flex-1 py-2 px-4 font-semibold cursor-pointer focus:outline-none peer-checked:bg-teal-500 peer-checked:text-white bg-white text-teal-500 transition-colors duration-200 {{ $loop->first ? 'border-r border-teal-500 rounded-l-md' : '' }} {{ $loop->last ? 'rounded-r-md' : '' }}">
+        {{ $optionLabel }}
+    </label>
+    @endforeach
 </div>
-@endforeach
