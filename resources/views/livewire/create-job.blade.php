08b55176a44805add2321b4b7e2cc67cce012635
Phuse21 <phusedallas@gmail.com>
2024-06-21 18:36:43 -0500

    spinners

--- resources/views/livewire/create-job.blade.php
+++ resources/views/livewire/create-job.blade.php
@@ -68,6 +68,14 @@
                 <p>By publishing you agree to our <a href="#" class="text-blue-500">Terms & Privacy</a></p>
 
                 <x-button type="submit">Publish</x-button>
+
+                <div wire:loading>
+                    <div class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                        role="status">
+                        <span
+                            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+                    </div>
+                </div>
             </form>
         </div>
 
--- resources/views/livewire/register-user.blade.php
+++ resources/views/livewire/register-user.blade.php
@@ -44,99 +44,107 @@
 
                 <!-- employer details -->
                 @if ($role == 'employer')
-                    <x-divider />
-                    <div class="flex space-x-4">
-                        <div class="w-1/2">
-                            <x-form.label :name="'employer'" :label="'Company Name'" />
-                            <x-form.input type="text" :name="'employer'" :id="'employer'" wire:model="employer" />
-                            <x-form.error name="employer" />
-                        </div>
-
-                        <div class="w-1/2">
-                            <x-form.label :name="'logo'" :label="'Company Logo'" />
-                            <x-form.input type="file" :name="'logo'" :id="'logo'" wire:model="logo" />
-                            <x-form.error name="logo" />
-                        </div>
+                <x-divider />
+                <div class="flex space-x-4">
+                    <div class="w-1/2">
+                        <x-form.label :name="'employer'" :label="'Company Name'" />
+                        <x-form.input type="text" :name="'employer'" :id="'employer'" wire:model="employer" />
+                        <x-form.error name="employer" />
                     </div>
+
+                    <div class="w-1/2">
+                        <x-form.label :name="'logo'" :label="'Company Logo'" />
+                        <x-form.input type="file" :name="'logo'" :id="'logo'" wire:model="logo" />
+                        <x-form.error name="logo" />
+                    </div>
+                </div>
                 @endif
 
 
                 <!-- applicant details -->
                 @if ($role == 'applicant')
-                    <x-divider />
-                    <div class="flex space-x-4">
-                        <div class="w-1/2">
-                            <x-form.label :name="'education'" :label="'Education'" />
-                            <x-form.input type="text" :name="'education'" :id="'education'" wire:model="education" />
-                            <x-form.error name="education" />
-                        </div>
-
-                        <div class="w-1/2">
-                            <x-form.label :name="'field'" :label="'Field'" />
-                            <x-form.input type="text" :name="'field'" :id="'field'" wire:model="field" />
-                            <x-form.error name="field" />
-                        </div>
-                    </div>
-                    <div class="flex space-x-4">
-                        <div class="w-1/2">
-                            <x-form.label :name="'country'" :label="'Country'" />
-                            <x-form.input type="text" :name="'country'" :id="'country'" wire:model="country" />
-                            <x-form.error name="country" />
-                        </div>
-
-                        <div class="w-1/2">
-                            <x-form.label :name="'phone'" :label="'Phone'" />
-                            <x-form.input type="text" :name="'phone'" :id="'phone'" wire:model="phone" />
-                            <x-form.error name="phone" />
-                        </div>
+                <x-divider />
+                <div class="flex space-x-4">
+                    <div class="w-1/2">
+                        <x-form.label :name="'education'" :label="'Education'" />
+                        <x-form.input type="text" :name="'education'" :id="'education'" wire:model="education" />
+                        <x-form.error name="education" />
                     </div>
 
-                    <div class="col-span-full">
-                        <x-form.label :name="'street-address'" :label="'Street Address'" />
-                        <x-form.input type="text" :name="'street-address'" :id="'street-address'"
-                            wire:model="street_address" />
-                        <x-form.error name="street_address" />
+                    <div class="w-1/2">
+                        <x-form.label :name="'field'" :label="'Field'" />
+                        <x-form.input type="text" :name="'field'" :id="'field'" wire:model="field" />
+                        <x-form.error name="field" />
+                    </div>
+                </div>
+                <div class="flex space-x-4">
+                    <div class="w-1/2">
+                        <x-form.label :name="'country'" :label="'Country'" />
+                        <x-form.input type="text" :name="'country'" :id="'country'" wire:model="country" />
+                        <x-form.error name="country" />
                     </div>
 
-                    <div class="flex space-x-4">
-                        <div class="w-1.5/4">
-                            <x-form.label :name="'city'" :label="'City'" />
-                            <x-form.input type="text" :name="'city'" :id="'city'" wire:model="city" />
-                            <x-form.error name="city" />
-                        </div>
+                    <div class="w-1/2">
+                        <x-form.label :name="'phone'" :label="'Phone'" />
+                        <x-form.input type="text" :name="'phone'" :id="'phone'" wire:model="phone" />
+                        <x-form.error name="phone" />
+                    </div>
+                </div>
 
-                        <div class="w-1.5/4">
-                            <x-form.label :name="'state'" :label="'State'" />
-                            <x-form.input type="text" :name="'state'" :id="'state'" wire:model="state" />
-                            <x-form.error name="state" />
-                        </div>
+                <div class="col-span-full">
+                    <x-form.label :name="'street-address'" :label="'Street Address'" />
+                    <x-form.input type="text" :name="'street-address'" :id="'street-address'"
+                        wire:model="street_address" />
+                    <x-form.error name="street_address" />
+                </div>
 
-                        <div class="w-1/4">
-                            <x-form.label :name="'zip'" :label="'Zip'" />
-                            <x-form.input type="text" :name="'zip'" :id="'zip'" wire:model="zip" />
-                            <x-form.error name="zip" />
-                        </div>
+                <div class="flex space-x-4">
+                    <div class="w-1.5/4">
+                        <x-form.label :name="'city'" :label="'City'" />
+                        <x-form.input type="text" :name="'city'" :id="'city'" wire:model="city" />
+                        <x-form.error name="city" />
                     </div>
 
-                    <div class="flex space-x-4">
-                        <div class="w-1/2">
-                            <x-form.label :name="'linkedin'" :label="'Linkedin'" />
-                            <x-form.input type="text" :name="'linkedin'" :id="'linkedin'" wire:model="linkedin" />
-                            <x-form.error name="linkedin" />
-                        </div>
-
-                        <div class="w-1/2">
-                            <x-form.label :name="'profile-photo'" :label="'Profile Photo'" />
-                            <x-form.input type="file" :name="'profile-photo'" :id="'profile-photo'"
-                                wire:model="profile_photo" />
-                            <x-form.error name="profile photo" />
-                        </div>
+                    <div class="w-1.5/4">
+                        <x-form.label :name="'state'" :label="'State'" />
+                        <x-form.input type="text" :name="'state'" :id="'state'" wire:model="state" />
+                        <x-form.error name="state" />
                     </div>
+
+                    <div class="w-1/4">
+                        <x-form.label :name="'zip'" :label="'Zip'" />
+                        <x-form.input type="text" :name="'zip'" :id="'zip'" wire:model="zip" />
+                        <x-form.error name="zip" />
+                    </div>
+                </div>
+
+                <div class="flex space-x-4">
+                    <div class="w-1/2">
+                        <x-form.label :name="'linkedin'" :label="'Linkedin'" />
+                        <x-form.input type="text" :name="'linkedin'" :id="'linkedin'" wire:model="linkedin" />
+                        <x-form.error name="linkedin" />
+                    </div>
+
+                    <div class="w-1/2">
+                        <x-form.label :name="'profile-photo'" :label="'Profile Photo'" />
+                        <x-form.input type="file" :name="'profile-photo'" :id="'profile-photo'"
+                            wire:model="profile_photo" />
+                        <x-form.error name="profile photo" />
+                    </div>
+                </div>
                 @endif
                 <p>Already have an account? <a wire:navigate href="/login" class="text-blue-500 font-bold">Login</a></p>
 
                 <p>By creating an account you agree to our <a href="#" class="text-blue-500">Terms & Privacy</a></p>
                 <x-button type="submit">Create Account</x-button>
+
+                <div wire:loading>
+                    <div class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                        role="status">
+                        <span
+                            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+                    </div>
+                </div>
             </form>
 
         </div>
