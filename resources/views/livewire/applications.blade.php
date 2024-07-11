9a106a65824a0d0c36f3bca48ef4b95a7934730d
Phuse21 <phusedallas@gmail.com>
2024-07-11 05:19:29 -0500

    linkedin link

--- resources/views/livewire/applications.blade.php
+++ resources/views/livewire/applications.blade.php
@@ -2,7 +2,22 @@
     <div class="h-fit w-full laptop:col-span-1 col-span-2 laptop:sticky laptop:top-20 flex flex-col items-center gap-5">
 
         <div
-            class="bg-gradient-to-t from-primary to-btn_gradient_blue/80  text-white h-fit w-full p-3 rounded-lg flex flex-col items-center border border-light_blue">
+            class="bg-gradient-to-t from-primary to-btn_gradient_blue/80 text-white h-fit w-full p-3 rounded-lg flex flex-col items-center border border-light_blue relative">
+
+            <!-- LinkedIn Icon -->
+            <a href="{{$this->application?->applicants?->linkedin ?? '#'}}" target="_blank"
+                class="absolute top-3 right-3">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" class="h-10 w-10">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path fill="#0A66C2"
+                            d="M12.225 12.225h-1.778V9.44c0-.664-.012-1.519-.925-1.519-.926 0-1.068.724-1.068 1.47v2.834H6.676V6.498h1.707v.783h.024c.348-.594.996-.95 1.684-.925 1.802 0 2.135 1.185 2.135 2.728l-.001 3.14zM4.67 5.715a1.037 1.037 0 01-1.032-1.031c0-.566.466-1.032 1.032-1.032.566 0 1.031.466 1.032 1.032 0 .566-.466 1.032-1.032 1.032zm.889 6.51h-1.78V6.498h1.78v5.727zM13.11 2H2.885A.88.88 0 002 2.866v10.268a.88.88 0 00.885.866h10.226a.882.882 0 00.889-.866V2.865a.88.88 0 00-.889-.864z">
+                        </path>
+                    </g>
+                </svg>
+            </a>
+
             <div class="h-32 w-32 rounded-full overflow-hidden flex items-center justify-center bg-white"
                 style="font-size: 50px;">
                 <img src="{{asset($application?->applicants?->profile_photo)}}" class="object-cover h-full w-full">
@@ -14,6 +29,7 @@
                 {{$this->application?->applicants?->user?->email ?? "N/A"}}
             </p>
         </div>
+
 
         <div
             class="bg-white p-3 text-black h-fit w-full rounded-lg flex flex-col items-center border border-light_blue">
@@ -33,7 +49,8 @@
             <div class="flex flex-col items-start justify-start border-t border-light_blue w-full p-2">
                 <p class="text-zinc-400 text-sm font-content w-full capitalize">Date of Birth</p>
                 <p class="text-base font-medium text-black font-content capitalize">
-                    {{$this->application?->applicants?->date_of_birth ?? "N/A"}}</p>
+                    {{$this->application?->applicants?->date_of_birth ?? "N/A"}}
+                </p>
             </div>
             <div class="flex flex-col items-start justify-start border-t border-light_blue w-full p-2">
                 <p class="text-zinc-400 text-sm font-content w-full capitalize">Country</p>
