e6cc2556a9b055577e03b43d3b15c8b8bac2705a
Phuse21 <phusedallas@gmail.com>
2024-07-31 14:49:53 -0500

    queue password reset mail

--- app/Notifications/CustomPasswordReset.php
+++ app/Notifications/CustomPasswordReset.php
@@ -1,26 +1,29 @@
 <?php
+
 namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
+use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class CustomPasswordReset extends ResetPasswordNotification
+class CustomPasswordReset extends ResetPasswordNotification implements ShouldQueue
 {
+    use Queueable;
     public $token;
 
     public function __construct($token)
     {
         $this->token = $token;
     }
+
     public function toMail($notifiable)
     {
-
         // Generate the URL for password reset
         $url = url(route('password.reset', [
             'token' => $this->token,
             'email' => $notifiable->getEmailForPasswordReset(),
         ], false));
-
 
         return (new MailMessage)
             ->subject('Password Reset')
