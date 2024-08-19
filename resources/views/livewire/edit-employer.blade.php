8d0b8c09eb5b690aa3486a602f8c1486d57a3a83
Phuse21 <phusedallas@gmail.com>
2024-08-19 09:03:19 -0400

    employer profile

--- resources/views/livewire/edit-employer.blade.php
+++ resources/views/livewire/edit-employer.blade.php
@@ -4,21 +4,21 @@
     <div class="flex space-x-2">
         <div class="flex-1">
             <form wire:submit="updateEmployer" class="max-w-2xl p-4 mx-auto space-y-6">
-                <div class="flex space-x-4">
-                    <div class="w-1/2">
+                <div class="grid md:grid-cols-2 grid-cols-1 gap-4">
+                    <div class="md:w-full w-[85%] mx-auto">
                         <x-form.label :name="'name'" :label="'Name'" />
                         <x-form.input type="text" :name="'name'" :id="'name'" wire:model="name" />
                         <x-form.error name="name" />
                     </div>
-                    <div class="w-1/2">
+                    <div class="md:w-full w-[85%] mx-auto">
                         <x-form.label :name="'employer'" :label="'Company Name'" />
                         <x-form.input type="text" :name="'employer'" :id="'employer'" wire:model="employer_name" />
                         <x-form.error name="employer" />
                     </div>
                 </div>
 
-                <div class="flex space-x-4">
-                    <div class="w-1/2">
+                <div class="grid md:grid-cols-2 grid-cols-1 gap-4">
+                    <div class="md:w-full w-[85%] mx-auto">
                         <x-form.label :name="'position'" :label="'Position in Company'" />
                         <x-form.select name="position" id="position" wire:model="position">
                             <option value="">--Please Select--</option>
@@ -31,20 +31,21 @@
                         <x-form.error name="position" />
                     </div>
 
-                    <div class="w-1/2">
+                    <div class="md:w-full w-[85%] mx-auto">
                         <x-form.label :name="'phone'" :label="'Phone'" />
                         <x-form.input type="text" :name="'phone'" :id="'phone'" wire:model="company_phone" />
                         <x-form.error name="phone" />
                     </div>
                 </div>
-                <div class="flex space-x-4">
-                    <div class="w-1/2">
+                <div class="grid md:grid-cols-2 grid-cols-1 gap-4">
+                    <div class="md:w-full w-[85%] mx-auto">
                         <x-form.label :name="'country'" :label="'Country'" />
-                        <x-form.input type="text" :name="'country'" :id="'country'" wire:model="company_country" />
+                        <x-form.input type="text" :name="'country'" :id="'country'"
+                            wire:model="company_country" />
                         <x-form.error name="country" />
                     </div>
 
-                    <div class="w-1/2">
+                    <div class="md:w-full w-[85%] mx-auto">
                         <x-form.label :name="'street-address'" :label="'Street Address'" />
                         <x-form.input type="text" :name="'street-address'" :id="'company-street-address'"
                             wire:model="company_street_address" />
@@ -53,50 +54,60 @@
                 </div>
 
 
-                <div class="flex space-x-4">
-                    <div class="w-1.5/4">
+                <div class="grid md:grid-cols-3 grid-cols-1 gap-4">
+                    <div class="md:w-full w-[85%] mx-auto">
                         <x-form.label :name="'state'" :label="'State'" />
                         <x-form.input type="text" :name="'state'" :id="'state'" wire:model="company_state" />
                         <x-form.error name="state" />
                     </div>
 
-                    <div class="w-1.5/4">
+                    <div class="md:w-full w-[85%] mx-auto">
                         <x-form.label :name="'city'" :label="'City'" />
                         <x-form.input type="text" :name="'city'" :id="'city'" wire:model="company_city" />
                         <x-form.error name="city" />
                     </div>
 
 
-                    <div class="w-1/4">
+                    <div class="md:w-full w-[85%] mx-auto">
                         <x-form.label :name="'zip'" :label="'Zip'" />
                         <x-form.input type="text" :name="'zip'" :id="'zip'" wire:model="company_zip" />
                         <x-form.error name="zip" />
                     </div>
                 </div>
-                <div class="flex space-x-4">
-                    <div class="w-1/2">
+                <div class="grid md:grid-cols-2 grid-cols-1 gap-4">
+                    <div class="md:w-full w-[85%] mx-auto">
                         <x-form.label :name="'linkedin'" :label="'Linkedin'" />
                         <x-form.input type="text" :name="'linkedin'" :id="'linkedin'" wire:model="url" />
                         <x-form.error name="linkedin" />
                     </div>
 
-                    <div class="w-1/2">
+                    <div class="md:w-full w-[85%] mx-auto">
                         <x-form.label :name="'logo'" :label="'Company Logo'" />
-                        <x-form.file :name="'logo'" :id="'logo'" wire:model="logo" />
+                        <x-form.file :name="'logo'" :id="'logo'" accept="image/*" wire:model="logo" />
                         <x-form.error name="logo" />
+                        <div class="flex justify-between">
+                            <small class="text-gray-500">Only images are accepted</small>
+                            <div wire:loading wire:target="logo" class="text-sm text-blue-500">
+                                Uploading...
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
-
-                <x-button type="submit">Update</x-button>
-
-                <div wire:loading>
-                    <div class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                        role="status">
-                        <span
-                            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
-                    </div>
+                <div class="md:w-full w-[85%] mx-auto">
+                    <x-button type="submit" wire:loading.attr="disabled" wire:target="logo,updateEmployer">
+                        Update
+                        <div wire:loading wire:target="updateEmployer">
+                            <div class="inline-block h-4 w-4 ml-1 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-gray-300 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                                role="status">
+                                <span
+                                    class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+                            </div>
+                        </div>
+                    </x-button>
                 </div>
+
             </form>
 
         </div>
--- resources/views/livewire/employer-profile.blade.php
+++ resources/views/livewire/employer-profile.blade.php
@@ -7,13 +7,13 @@
                     class="bg-gradient-to-t from-primary to-btn_gradient_blue/80 text-white h-fit w-full p-3 rounded-lg flex flex-col items-center border border-light_blue relative">
                     <div class="h-32 w-32 p-1 rounded-full overflow-hidden flex items-center justify-center bg-white"
                         style="font-size: 50px;">
-                        <img src="{{asset($employer?->logo ?? 'https://i.ibb.co/Yk3VRHC/user-131563.png')}}"
+                        <img src="{{ asset($employer?->logo ?? 'https://i.ibb.co/Yk3VRHC/user-131563.png') }}"
                             class="object-cover rounded-full h-full w-full">
                     </div>
                     <p class="text-white text-xl font-bold font-content capitalize mt-3">
-                        {{$employer?->user?->name ?? 'N/A'}}
+                        {{ $employer?->user?->name ?? 'N/A' }}
                     </p>
-                    <p class="text-base font-light text-white font-content">{{$employer?->user?->email ?? 'N/A'}}</p>
+                    <p class="text-base font-light text-white font-content">{{ $employer?->user?->email ?? 'N/A' }}</p>
                 </div>
 
                 <div
@@ -21,26 +21,37 @@
                     <div class="flex flex-col items-start justify-start border-light_blue w-full p-2">
                         <p class="text-zinc-400 text-sm font-content w-full capitalize">Total Jobs Posted</p>
                         <p class="text-base font-medium text-black font-content capitalize">
-                            {{$jobs?->count() ?? 'N/A'}}
+                            {{ $jobs?->count() ?? 'N/A' }}
                         </p>
                     </div>
                     <div class="flex flex-col items-start justify-start border-t border-light_blue w-full p-2">
                         <p class="text-zinc-400 text-sm font-content w-full capitalize">Active Jobs</p>
                         <p class="text-base font-medium text-black font-content capitalize">
-                            {{$employer?->jobs?->count() ?? 'N/A'}}
+                            {{ $employer?->jobs?->count() ?? 'N/A' }}
+                        </p>
+                    </div>
+                    <div class="flex flex-col items-start justify-start border-t border-light_blue w-full p-2">
+                        <p class="text-zinc-400 text-sm font-content w-full capitalize">Accepted Applications</p>
+                        <p class="text-base font-medium text-black font-content capitalize">
+                            {{ $applications?->where('status', 'accepted')->count() ?? 'N/A' }}
+                        </p>
+                    </div>
+                    <div class="flex flex-col items-start justify-start border-t border-light_blue w-full p-2">
+                        <p class="text-zinc-400 text-sm font-content w-full capitalize">Declined Applications</p>
+                        <p class="text-base font-medium text-black font-content capitalize">
+                            {{ $applications?->where('status', 'declined')->count() ?? 'N/A' }}
                         </p>
                     </div>
                     <div class="flex flex-col items-start justify-start border-t border-light_blue w-full p-2">
                         <p class="text-zinc-400 text-sm font-content w-full capitalize">Member Since</p>
                         <p class="text-base font-medium text-black font-content capitalize">
-                            {{Carbon\Carbon::parse($employer?->created_at)->format('F j, Y') ?? 'N/A'}}
+                            {{ Carbon\Carbon::parse($employer?->created_at)->format('F j, Y') ?? 'N/A' }}
                         </p>
                     </div>
                     <div class="flex flex-col items-start justify-start border-t border-light_blue w-full p-2">
                         <p class="text-zinc-400 text-sm font-content w-full capitalize">Socials</p>
-                        <div class="flex items-center justify-between w-full">
-                            Linkedin
-                            <a href="{{$employer?->url ?? '#'}}" target="_blank" class="flex flex-row items-center">
+                        <div class="flex items-center w-full">
+                            <a href="{{ $employer?->url ?? '#' }}" target="_blank" class="flex flex-row items-center">
                                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none"
                                     class="h-12 w-12">
                                     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
@@ -58,7 +69,7 @@
                         <p class="text-zinc-400 text-sm font-content w-full capitalize">Position</p>
                         <div class="flex items-center justify-between w-full">
                             <p class="text-base font-medium text-black font-content capitalize">
-                                {{$employer?->position ?? 'N/A'}}
+                                {{ $employer?->position ?? 'N/A' }}
                             </p>
                         </div>
                     </div>
@@ -79,49 +90,49 @@
 
                         <label class="w-1/3" title="Company Name">Company Name</label>
                         <div class="w-2/3 p-2 text-base text-left border rounded-lg h-fit border-gray2/50 break-words">
-                            {{$employer?->name ?? 'N/A'}}
+                            {{ $employer?->name ?? 'N/A' }}
                         </div>
                     </div>
                     <div class="flex text-sm items-center capitalize text-black">
 
                         <label class="w-1/3" title="Nationality">Country</label>
                         <div class="w-2/3 p-2 text-base text-left border rounded-lg h-fit border-gray2/50 break-words">
-                            {{$employer?->company_country ?? 'N/A'}}
+                            {{ $employer?->company_country ?? 'N/A' }}
                         </div>
                     </div>
                     <div class="flex text-sm items-center capitalize text-black">
 
                         <label class="w-1/3" title="Nationality">Street address</label>
                         <div class="w-2/3 p-2 text-base text-left border rounded-lg h-fit border-gray2/50 break-words">
-                            {{$employer?->company_street_address ?? 'N/A'}}
+                            {{ $employer?->company_street_address ?? 'N/A' }}
                         </div>
                     </div>
                     <div class="flex text-sm items-center capitalize text-black">
 
                         <label class="w-1/3" title="Nationality">State/province</label>
                         <div class="w-2/3 p-2 text-base text-left border rounded-lg h-fit border-gray2/50 break-words">
-                            {{$employer?->company_state ?? 'N/A'}}
+                            {{ $employer?->company_state ?? 'N/A' }}
                         </div>
                     </div>
                     <div class="flex text-sm items-center capitalize text-black">
 
                         <label class="w-1/3" title="Nationality">City</label>
                         <div class="w-2/3 p-2 text-base text-left border rounded-lg h-fit border-gray2/50 break-words">
-                            {{$employer?->company_city ?? 'N/A'}}
+                            {{ $employer?->company_city ?? 'N/A' }}
                         </div>
                     </div>
                     <div class="flex text-sm items-center capitalize text-black">
 
                         <label class="w-1/3" title="Nationality">Phone Number</label>
                         <div class="w-2/3 p-2 text-base text-left border rounded-lg h-fit border-gray2/50 break-words">
-                            {{$employer?->company_phone ?? 'N/A'}}
+                            {{ $employer?->company_phone ?? 'N/A' }}
                         </div>
                     </div>
                     <div class="flex text-sm items-center capitalize text-black">
 
                         <label class="w-1/3" title="Nationality">Zip</label>
                         <div class="w-2/3 p-2 text-base text-left border rounded-lg h-fit border-gray2/50 break-words">
-                            {{$employer?->company_zip ?? 'N/A'}}
+                            {{ $employer?->company_zip ?? 'N/A' }}
                         </div>
                     </div>
                 </form>
